8cd6796fdc9b1b0fef4a190ccd4cb14cff4e3591
Laurent Valdes <valderama@gmail.com>
2025-01-07 23:48:57 +0100

    fix: some plays with proxy pacs

--- src/pac.rs
+++ src/pac.rs
@@ -1,14 +1,13 @@
-use std::fs;
-use std::path::Path;
-use boa_engine::{Context, Source};
-use boa_engine::ast::scope::Scope;
-use boa_engine::ast::{Declaration, Expression, Script, Statement, StatementList, StatementListItem};
 use boa_engine::ast::expression::literal::Literal;
-use boa_engine::ast::function::FunctionBody;
+use boa_engine::ast::scope::Scope;
 use boa_engine::ast::statement::{If, Return};
+use boa_engine::ast::{Declaration, Expression, Script, Statement, StatementList, StatementListItem};
 use boa_engine::interner::Interner;
 use boa_engine::parser::Parser;
+use boa_engine::Source;
 use boa_parser::error::ParseResult;
+use std::fs;
+use std::path::Path;
 
 #[derive(Debug, Clone)]
 pub enum PacExpression {
@@ -27,7 +26,6 @@ pub enum PacCondition {
 }
 
 fn build_pac_expression(script: Script, interner: &Interner) -> PacExpression {
-    // Process the Script's StatementList
     process_statement_list(script.statements(), interner)
 }
 
@@ -74,6 +72,16 @@ fn parse_return(interner: &Interner, return_stmt: &Return) -> Option<PacExpressi
                 let resolved_proxy_string = interner.resolve(*proxy_string)?.utf8().unwrap();
                 println!("Resolved proxy string: {:?}", resolved_proxy_string);
 
+                // TODO PROXY w3proxy.netscape.com:8080; PROXY mozilla.netscape.com:8081
+                // TODO PROXY w3proxy.netscape.com:8080; PROXY mozilla.netscape.com:8081; DIRECT
+                // TODO PROXY w3proxy.netscape.com:8080; SOCKS socks:1080
+                // TODO DIRECT
+                // TODO PROXY host:port
+                // TODO SOCKS host:port
+                // TODO HTTP host:port
+                // TODO HTTPS host:port
+                // TODO SOCKS4 host:port, SOCKS5 host:port
+
                 if resolved_proxy_string.contains("PROXY") {
                     return Some(PacExpression::Proxy(resolved_proxy_string.clone().to_string()));
                 } else if resolved_proxy_string == "DIRECT" {
@@ -89,17 +97,36 @@ fn parse_if_method(interner: &Interner, if_stmt: &&If) -> Option<PacExpression> 
     println!("Processing if statement: {:?}", if_stmt);
     println!("If condition: {:?}", if_stmt.cond());
     println!("If body: {:?}", if_stmt.body());
+    println!("If else node: {:?}", if_stmt.else_node());
 
     let condition = Box::new(parse_condition(&if_stmt.cond()));
     println!("Pac condition: {:?}", condition);
 
     let body = if let Statement::Block(block) = if_stmt.body() {
         println!("Processing block statement: {:?}", block);
-
+        // TODO PROXY w3proxy.netscape.com:8080; PROXY mozilla.netscape.com:8081
+        // TODO PROXY w3proxy.netscape.com:8080; PROXY mozilla.netscape.com:8081; DIRECT
+        // TODO PROXY w3proxy.netscape.com:8080; SOCKS socks:1080
+        // TODO DIRECT
+        // TODO PROXY host:port
+        // TODO SOCKS host:port
+        // TODO HTTP host:port
+        // TODO HTTPS host:port
+        // TODO SOCKS4 host:port, SOCKS5 host:port
         unimplemented!();
         // Box::new(process_statement_list(block)) // Process as a statement list
     } else if let Statement::Return(return_statement) = if_stmt.body() {
         println!("Processing return statement: {:?}", return_statement);
+
+        // TODO PROXY w3proxy.netscape.com:8080; PROXY mozilla.netscape.com:8081
+        // TODO PROXY w3proxy.netscape.com:8080; PROXY mozilla.netscape.com:8081; DIRECT
+        // TODO PROXY w3proxy.netscape.com:8080; SOCKS socks:1080
+        // TODO DIRECT
+        // TODO PROXY host:port
+        // TODO SOCKS host:port
+        // TODO HTTP host:port
+        // TODO HTTPS host:port
+        // TODO SOCKS4 host:port, SOCKS5 host:port
 
         unimplemented!();
     } else {
@@ -126,6 +153,26 @@ fn process_find_proxy(declaration: &Declaration, interner: &Interner) -> Option<
 fn parse_condition(condition: &Expression) -> PacCondition {
     println!("Condition: {:?}", condition);
 
+
+    // TODO parse isPlainHostName
+    // TODO parse dnsDomainIs
+    // TODO parse localHostOrDomainIs()
+    // TODO parse dnsResolve()
+    // TODO parse convert_addr()
+    // TODO parse dnsDomainLevels()
+    // TODO parse shExpMatch()
+    // TODO parse weekdayRange()
+    // TODO parse dateRange()
+    // TODO parse timeRange()
+    // TODO parse host == "download.microsoft.com"
+    // TODO parse isResolvable
+    // TODO parse isInNet
+    // TODO parse true / false
+    // TODO url.substring(0, 5) == 'http:'
+    // TODO url.substring(0, 6) == 'https:'
+    // TODO url.substring(0, 4) == 'ftp:'
+    // TODO parse ||
+    // TODO parse &&
     match condition {
         Expression::Call(call_expr) => {
             unimplemented!();
@@ -152,28 +199,19 @@ fn parse_condition(condition: &Expression) -> PacCondition {
     PacCondition::Always
 }
 
-#[test]
-fn test_pac_file_parsing() {
-    // Path to the PAC file
-    let pac_file_path = "./proxy.pac";
-
-    // Check if the PAC file exists
+fn test_pac_file_parsing_with_file(pac_file_path: &str) {
     assert!(
         Path::new(pac_file_path).exists(),
         "PAC file not found at {}",
         pac_file_path
     );
 
-    // Read the file
     let pac_code = fs::read_to_string(pac_file_path).expect("Failed to read proxy.pac file");
 
-    // Set up the Boa parser internals
     let mut interner = Interner::default();
     let source = Source::from_bytes(pac_code.as_bytes());
 
-    // Parse the source code and generate the AST
     let parse_result: ParseResult<Script> = Parser::new(source).parse_script(&Scope::new_global(), &mut interner);
-    // Assert that parsing succeeded
     assert!(
         parse_result.is_ok(),
         "PAC file parsing failed: {:?}",
@@ -186,4 +224,18 @@ fn test_pac_file_parsing() {
         // Output the resulting PacExpression for debugging
         println!("PacExpression:\n{:#?}", pac_expression);
     }
+}
+
+#[test]
+fn test_pac_file_parsing() {
+    // Pass the file path to the parameterized function
+    test_pac_file_parsing_with_file("./proxy.pac");
+    test_pac_file_parsing_with_file("./full-proxy.pac");
+    test_pac_file_parsing_with_file("./complicated.pac");
+
+}
+
+#[test]
+fn test_direct_pac_execution() {
+    // TODO directly call the pac method
 }
